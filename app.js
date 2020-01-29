0f293663151de556398422ed7f53dafc28f7ce73
ihalilsungur <ihalilsungur@gmail.com>
2020-01-29 15:02:35 +0300

    javascript-with-budgety projemizin arayüzünden girilen değerleri UIController fonksiyonu içinde bir nesne olarak geri dönderiyoruz. Bu geriye dönderilen nesneyi controller içinde ctrlAddItem fonksiyonu içinde elde ediyoruz.

--- app.js
+++ app.js
@@ -2,26 +2,48 @@
 Burada function () içinde aldığımızda dışarıdan irişimi engeliyoruz
  */
 let budgetController = (function () {
-    let x = 23;
-    let add = function (a) {
-        return x + a;
-    };
+
+})();
+
+/*Arayuz guncelleme controlleri modülünün tanımı
+ */
+let UIController = (function () {
     return {
-        publicTest: function (b) {
-            console.log(add(b));
+        getInput: function () {
+            //Girilen değerleri nesne olarak geri dönderiyoruz;
+            return {
+                type: document.querySelector(".add__type").value,    //.add__type gelecek değerler inc ve exp
+                description: document.querySelector(".add__description").value,
+                value: document.querySelector(".add__value").value,
+            };
         }
     }
 })();
 
-/*Arayuz guncelleme controlleri modülünün tanımı
-
- */
-let UIController = (function () {
-
-})();
-
 /*budgetcontroller modülü  ile UIController  modülünü birleştireceğimiz controller modülü
  */
-let controller = (function (budgetCtrl,UICtrl) {
-budgetCtrl.publicTest(7);
-})(budgetController,UIController);
+let controller = (function (budgetCtrl, UICtrl) {
+
+    let ctrlAddItem = function () {
+        //1. Girilen değerleri al.
+        let input = UICtrl.getInput();
+        console.log(input);
+
+        //2.itemleri bütçe denetleyicisine ekle.
+
+        //3.Eklenen öğeyi UIController ekle.
+
+        //4.Bütçeyi hesapla.
+
+        //5.Bütçeyi UIController arayüzüne gönder.
+
+
+    };
+    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+
+    document.addEventListener("keypress", function (event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+})(budgetController, UIController);
